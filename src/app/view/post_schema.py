799277808d782e55c0249f42aae9069f91cfb5bd
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-03-20 19:52:51 -0300

    (#101) criando schema do Post

---
+++ src/app/view/post_schema.py
@@ -0,0 +1,15 @@
+from . import ma
+from marshmallow import fields, validate, validates, ValidationError
+from ..model.dao import post_dao
+
+
+class PostSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = post_dao.Post
+
+    reg_student = fields.Integer(required=True, validate=validate.Range(min=0))
+    reg_professor = fields.Integer(required=True, validate=validate.Range(min=0))
+    discipline_code = fields.Integer(required=True, validate=validate.Range(min_inclusive=0))
+    content = fields.String(required=True, validate=validate.Length(min=0, max=480))
+    rating = fields.Float(required=True, validate=validate.Range(min_inclusive=0, max_inclusive=10))
+
