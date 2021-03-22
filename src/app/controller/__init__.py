01bffa4fb28a4da425e4b7e88dac96877bed4586
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-03-22 17:30:08 -0300

    (#101) criando rota para cadastro de Post

--- src/app/controller/__init__.py
+++ src/app/controller/__init__.py
@@ -11,10 +11,12 @@ def init_app(app):
     auth.init_app(app)
     cors.init_app(app)
 
-    from . import home_resources, course_resources, student_resources
+    from . import home_resources, course_resources, student_resources, post_resources
     api.add_resource(home_resources.HomeResource, "/")
     api.add_resource(course_resources.CourseList, "/course")
     api.add_resource(student_resources.StudentList, "/student")
+    api.add_resource(post_resources.PostList, "/post")
+
     app.register_blueprint(bp)
 
     @app.route("/")
