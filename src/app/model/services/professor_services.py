f75199cf2da481bf87a9a2f55064f785dda27934
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-03-22 19:07:41 -0300

    (#102) criando metodo para pesquisa de professor por parte do nome

--- src/app/model/services/professor_services.py
+++ src/app/model/services/professor_services.py
@@ -1,6 +1,11 @@
 from ..dao import professor_dao
 
 
+def get_professor_name_contains(name):
+    professors = professor_dao.Professor.query.filter(professor_dao.Professor.name.contains(name)).all()
+    return professors
+
+
 def get_professor_id(id_professor):
     professor = professor_dao.Professor.query.filter_by(id_professor=id_professor).first()
     return professor
