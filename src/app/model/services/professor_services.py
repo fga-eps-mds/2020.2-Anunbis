f249b8b30c8f224455b3a436151eb535604b952a
RcleydsonR <rafael.cleydson@gmail.com>
2021-03-23 22:16:06 -0300

    (#91) Adicionando return a funcao get_professor_reg

--- src/app/model/services/professor_services.py
+++ src/app/model/services/professor_services.py
@@ -26,7 +26,7 @@ def register_professor(professor):
 
 def get_professor_reg(reg_professor):
     professor = professor_dao.Professor.query.filter_by(reg_professor=reg_professor).first()
-
+    return professor
 
 def get_professor_id(id_professor):
     professor = professor_dao.Professor.query.filter_by(id_professor=id_professor).first()
