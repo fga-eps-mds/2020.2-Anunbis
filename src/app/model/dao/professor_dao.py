4262db6e630bdd038638d9d2f962af73186a3353
RcleydsonR <rafael.cleydson@gmail.com>
2021-04-07 14:34:35 -0300

    (#148) Alterando nome da tabela para minusculos
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>

--- src/app/model/dao/professor_dao.py
+++ src/app/model/dao/professor_dao.py
@@ -3,16 +3,16 @@ from passlib.hash import pbkdf2_sha256
 
 
 class Professor(db.Model):
-    __tablename__ = "PROFESSOR"
+    __tablename__ = "professor"
 
-    id_professor = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_professor = db.Column(db.Integer, primary_key=True, )
     reg_professor = db.Column(db.Integer, nullable=True, unique=True)
 
     name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=True, unique=True)
     password = db.Column(db.String(255), nullable=True)
 
-    disciplines = db.relationship('Discipline', secondary='PROFESSOR_DISCIPLINE', lazy='dynamic')
+    disciplines = db.relationship('Discipline', secondary='professor_discipline', lazy='dynamic')
 
     posts = db.relationship('Post', back_populates="professor")
 
