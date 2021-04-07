6622e6b984b9faa61e702a0d06a57a104f11f1d2
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-04-07 17:10:39 -0300

    (#148) adicionando verificacao para aluno nao existente

--- src/app/model/services/login_services.py
+++ src/app/model/services/login_services.py
@@ -8,7 +8,7 @@ def auth_student(email, password):
     student_db = student_services.get_student_email(email)
     ss = StudentSchema(only=['reg_student', 'name', 'email', 'id_course'])
 
-    if student_db.verify_password(password):
+    if student_db and student_db.verify_password(password):
         access_token = create_access_token(
             identity=student_db.reg_student,
             expires_delta=timedelta(minutes=20)
