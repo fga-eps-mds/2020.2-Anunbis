2637e72685ab98e5532ef84a50ccebc7ed720eac
Rodrigo Balbino <rodrigo080599@hotmail.com>
2021-04-08 19:50:07 -0300

    (#125) Adicionando o ProfessorSchema no login schema

--- app/schemas/login_schema.py
+++ app/schemas/login_schema.py
@@ -1,13 +1,14 @@
 from . import ma
 from marshmallow import fields, validate, validates, ValidationError
 from ..model.student import Student
+from ..model.professor import Student
 from re import match
 
 
-class LoginSchema(ma.SQLAlchemySchema):
+class LoginStudentSchema(ma.SQLAlchemySchema):
     class Meta:
-        model = Student
-        fields = ['id', 'name', 'email', 'password']
+        model = student_dao.Student
+        fields = ['email', 'password']
 
     email = fields.Email(required=True)
     password = fields.String(required=True, validate=validate.Length(min=8, max=100))
@@ -18,3 +19,20 @@ class LoginSchema(ma.SQLAlchemySchema):
             raise ValidationError("The email must be lower than 100")
         elif not match("[0-9]+@aluno.unb.br", value.lower()):
             raise ValidationError("The email must be matricula@aluno.unb.br")
+
+class LoginProfessorSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = professor_dao.Professor
+        fields = ['email','password']
+    
+    email = fields.Email(required=True)
+    password = fields.String(required=True, validate=validate.Length(min=8, max=100))
+
+    @validates("email")
+    def validate_email(self, value):
+        if len(value) > 100:
+            raise ValidationError("The email must be lower than 100")
+        elif not match("[0-9]+@unb.br", value.lower()):
+            raise ValidationError("The email must be matricula@unb.br")
+
+
