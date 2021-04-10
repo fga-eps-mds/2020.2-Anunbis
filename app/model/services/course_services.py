200bfda7deadce5472f1adb2128c30aa9a7cf0f6
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-04-10 17:29:05 -0300

    (#157) refatorando entidade Course Foi-se refatorado e adicionado um metodo construtor que recebe uma entidade do banco de dados course_bd

--- app/model/services/course_services.py
+++ app/model/services/course_services.py
@@ -1,11 +1,13 @@
 from ..dao import course_dao
+from ..entity.course import Course
+
 
 def get_course():
-    courses = course_dao.Course.query.all()
+    courses_bd = course_dao.Course.query.all()
+    courses = [Course(course_bd=course_bd) for course_bd in courses_bd]
     return courses
 
 
 def get_course_id(id):
-    course = course_dao.Course.query.filter_by(id_course=id).first()
-    return course
-
+    course_bd = course_dao.Course.query.filter_by(id_course=id).first()
+    return Course(course_bd=course_bd)
