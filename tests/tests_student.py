bf968f5da6ac1ffdda990bd74cdda4842d2ca1b4
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-04-11 13:45:37 -0300

    (#157) adicionando testes de validacao de atributo

--- tests/tests_student.py
+++ tests/tests_student.py
@@ -7,15 +7,19 @@ class TestStudentList(TestFlaskBase):
     def post(self, json):
         return self.client.post(url_for("restapi.studentlist"), json=json)
 
-    def test_api_must_register(self):
-        self.create_base_course()
-        student = {
+    def valid_student(self):
+        return {
             "name": "Testing Student",
             "reg_student": "190020000",
             "id_course": "1",
             "email": "190020000@aluno.unb.br",
             "password": "password"
         }
+
+    def test_api_must_register(self):
+        self.create_base_course()
+        student = self.valid_student()
+
         response = self.post(student)
         self.assertEqual(response.status_code, 201)
 
@@ -27,39 +31,66 @@ class TestStudentList(TestFlaskBase):
         self.assertEqual(list(response.json.keys()), expected)
 
     def test_must_validate_string_attributes(self):
-        student = {
-            "name": "",
-            "reg_student": "190020000",
-            "id_course": "1",
-            "email": 123,
-            "password": 123456789
-        }
+        student = self.valid_student()
+        student["name"] = ""
+        student["email"] = 123
+        student["password"] = 123456789 
+ 
         response = self.post(student)
         self.assertIsNotNone(response.json['name'])
         self.assertIsNotNone(response.json['email'])
         self.assertIsNotNone(response.json['password'])
 
     def test_must_validate_integer_attributes(self):
-        student = {
-            "name": "Testing Student",
-            "reg_student": "19002asdasadssd0000",
-            "id_course": "asddsaadadad1",
-            "email": "190020000@aluno.unb.br",
-            "password": "password"
-        }
+        student = self.valid_student()
+        student['id_course'] = "adsasadasdassd"
+        student['reg_student'] = "asddasasddas"
+
         response = self.post(student)
         self.assertIsNotNone(response.json['id_course'])
         self.assertIsNotNone(response.json['reg_student'])
 
     def test_must_validate_course_not_found(self):
-        student = {
-            "name": "Testing Student",
-            "reg_student": "190020000",
-            "id_course": 2,
-            "email": "190020000@aluno.unb.br",
-            "password": "123456789"
-        }
+        student = self.valid_student()
+        student['id_course'] = 10
+
         response = self.post(student)
         self.assertEqual(response.status_code, 404)
+        self.assertIsNot(response.json['message'].lower().find("course"), -1)
 
+    def test_must_not_register_duplicated(self):
+        self.create_base_student()
+        expected_status = 409
 
+        response = self.post(self.student)
+
+        self.assertEqual(response.status_code, expected_status)
+
+    def test_must_validate_email_len_greater_than_100(self):
+        email = '1'*100 + "@aluno.unb.br"
+        student = self.valid_student()
+        student['email'] = email
+
+        response = self.post(student)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['email'][0], "The email must be lower than 100")
+
+    def test_must_validate_email_format(self):
+        email = "adsasdsa@gmail.com"
+        expected = "The email must be matricula@aluno.unb.br"
+        student = self.valid_student()
+        student['email'] = email
+
+        response = self.post(student)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['email'][0], "The email must be matricula@aluno.unb.br")
+    
+    def test_must_validate_negative_integer(self):
+        student = self.valid_student()
+        student['reg_student'] = -1
+        student['id_course'] = -1
+
+        response = self.post(student)
+        self.assertEqual(response.status_code, 400)
+        self.assertIsNotNone(response.json['reg_student'])
+        self.assertIsNotNone(response.json['id_course'])
