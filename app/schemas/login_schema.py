dbd050152dff85c0b3c5806a90668b510ccfe40d
Rodrigo Balbino <rodrigo080599@hotmail.com>
2021-04-12 18:20:09 -0300

    (#125) Unificando schemas que validam email do login

--- app/schemas/login_schema.py
+++ app/schemas/login_schema.py
@@ -5,7 +5,7 @@ from ..model.professor import Student
 from re import match
 
 
-class LoginStudentSchema(ma.SQLAlchemySchema):
+class LoginSchema(ma.SQLAlchemySchema):
     class Meta:
         model = student_dao.Student
         fields = ['email', 'password']
@@ -17,22 +17,11 @@ class LoginStudentSchema(ma.SQLAlchemySchema):
     def validate_email(self, value):
         if len(value) > 100:
             raise ValidationError("The email must be lower than 100")
-        elif not match("[0-9]+@aluno.unb.br", value.lower()):
-            raise ValidationError("The email must be matricula@aluno.unb.br")
-
-class LoginProfessorSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = professor_dao.Professor
-        fields = ['email','password']
-    
-    email = fields.Email(required=True)
-    password = fields.String(required=True, validate=validate.Length(min=8, max=100))
-
-    @validates("email")
-    def validate_email(self, value):
-        if len(value) > 100:
-            raise ValidationError("The email must be lower than 100")
-        elif not match("[0-9]+@unb.br", value.lower()):
-            raise ValidationError("The email must be matricula@unb.br")
+        if value.lower().find("aluno") is not -1:
+            if not match("[0-9]+@aluno.unb.br", value.lower()):
+                raise ValidationError("The email must be matricula@aluno.unb.br")
+        else:
+            if not match("[0-9]+@unb.br", value.lower()):
+                raise ValidationError("The email must be matricula@unb.br")
 
 
