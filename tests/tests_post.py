a3af38af1e7f0eb004b08e58483afeb7fc6d422a
Rodrigo Balbino <rodrigo080599@hotmail.com>
2021-04-18 16:55:46 -0300

    (#137) Adicionando testes iniciais dos posts agree e disagree

--- tests/tests_post.py
+++ tests/tests_post.py
@@ -1,6 +1,44 @@
 from flask_base_tests_cases import TestFlaskBase
 from flask import url_for
 
+
+def valid_post(self):
+    self.create_base_entities()
+    return {
+        "id_post": 1,
+        "id_professor": self.professor['id_professor'],
+        "reg_student": self.student['reg_student'],
+        "discipline_code": self.discipline['discipline_code'],
+        "content": "Professor nota dez",
+        "rating": "5",
+        "is_anonymous": True
+    } 
+
+def register_post(self, post=None, headers=None):
+    if headers is None:
+        headers = self.create_student_token()
+    if post is None:
+        post = valid_post(self)
+
+    return self.client.post(url_for('restapi.postlist'), json=post, headers=headers)
+
+def register_post_agree(self, post=None, headers=None):
+    if headers is None:
+        headers = self.create_student_token()
+    if post is None:
+        post = valid_post(self)
+
+    return self.client.post(url_for('restapi.postagreeslist'), json=post, headers=headers)
+
+def register_post_disagree(self, post=None, headers=None):
+    if headers is None:
+        headers = self.create_student_token()
+    if post is None:
+        post = valid_post(self)
+
+    return self.client.post(url_for('restapi.postdisagreeslist'), json=post, headers=headers)
+
+
 class TestPostList(TestFlaskBase):
 
     def test_api_must_register_a_valid_post(self):
@@ -23,7 +61,7 @@ class TestPostList(TestFlaskBase):
 
         response = register_post(self, post=post)
 
-        expected_json_keys = ['content', 'discipline_code', 'id_professor', 'is_anonymous', 'rating', 'reg_student']
+        expected_json_keys = ['content', 'discipline_code', 'id_post','id_professor', 'is_anonymous', 'rating', 'reg_student']
         json_keys = list(response.json.keys())
         self.assertEqual(response.status_code, 400)
         self.assertEqual(json_keys, expected_json_keys)
@@ -78,21 +116,38 @@ class TestPostList(TestFlaskBase):
         self.assertEqual(response.json['message'], "Discipline not found!")
 
 
-def valid_post(self):
-    self.create_base_entities()
-    return {
-        "id_professor": self.professor['id_professor'],
-        "reg_student": self.student['reg_student'],
-        "discipline_code": self.discipline['discipline_code'],
-        "content": "Professor nota dez",
-        "rating": "5",
-        "is_anonymous": True
-    }  
-
-def register_post(self, post=None, headers=None):
-    if headers is None:
-        headers = self.create_student_token()
-    if post is None:
+class TestPostAgree(TestFlaskBase):
+    def test_api_must_register_student_agree(self):
         post = valid_post(self)
+        register_post(self, post=post)
 
-    return self.client.post(url_for('restapi.postlist'), json=post, headers=headers)
+        response = register_post_agree(self)
+        self.assertEqual(response.status_code, 200)
+        # verificar se o numero de curtida = 1
+
+    def test_api_must_unagree_post(self):
+        post = valid_post(self)
+        register_post(self, post=post)
+
+        register_post_agree(self)
+        response = register_post_agree(self)
+        self.assertEqual(response.status_code, 200)
+        # verificar se o numero de curtida == 0
+
+class TestPostDisagree(TestFlaskBase):
+
+    def test_api_must_register_student_disagree(self):
+        post = valid_post(self)
+        register_post(self, post=post)
+        response = register_postDisagree(self)
+        self.assertEqual(response.status_code, 200)
+        # verificar se o numero de agree = 1
+
+
+    def test_api_must_undisagree_post(self):
+        post = valid_post(self)
+        register_post(self, post=post)
+        register_post_disagree(self)
+        response = register_postDisagree(self)
+        self.assertEqual(response.status_code, 200)
+        # verificar se o numero de disagree = 1
