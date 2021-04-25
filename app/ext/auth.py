6cd1c869219560b2fb646351378f2325ac257b03
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-04-25 19:44:35 -0300

    (#125) adicionando metodo que retorna o usuario

--- app/ext/auth.py
+++ app/ext/auth.py
@@ -1,4 +1,6 @@
 from flask_jwt_extended import JWTManager
+from ..model import user
+
 
 jwt = JWTManager()
 
@@ -10,3 +12,9 @@ def init_app(app):
 @jwt.user_identity_loader
 def user_identity_lookup(user):
     return user.reg
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return user.get(reg=identity)
--- app/model/user.py
+++ app/model/user.py
@@ -1,11 +1,33 @@
+from ..model import student, professor
 
 
 class User:
-    def __init__(self, email=None, password=None):
+    def __init__(self, email, password):
         self.email = email
         self.password = password
 
-    def is_professor(self, reg=None):
+    def is_professor(self):
+        return is_professor(email=self.email)
+
+    def get(self):
+        return get(email=self.email)
+
+
+def is_professor(email=None, reg=None):
+    return (
+        len(str(reg)) == 11
+        if reg
+        else not "aluno" in email.lower()
+    )
+
+
+def get(reg=None, email=None):
+    if is_professor(reg=reg, email=email):
         if reg:
-            return len(reg) == 11
-        return not "aluno" in self.email.lower()
+            return professor.Professor.query.filter_by(reg_professor=reg).one_or_none()
+        return professor.Professor.query.filter_by(email=email).one_or_none()
+    else:
+        if reg:
+            return student.Student.query.filter_by(reg_student=reg).one_or_none()
+        else:
+            return student.Student.query.filter_by(email=email).one_or_none()
