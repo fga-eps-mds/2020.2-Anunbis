19e2eab7bab5363fd1f1ce3f2c4c949d2d381050
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-04-27 23:44:51 -0300

    (#138) Alterando campos de denuncia

--- app/model/report.py
+++ app/model/report.py
@@ -1,19 +1,16 @@
 from app.ext.database import db
-import enum
-
-
-class ReportTypes(enum.Enum):
-    linguagem_ofensiva = 'L'
-    incoerencia = 'I'
-    grave = 'G'
-    outros = 'O'
 
 
 class Report(db.Model):
     __tablename__ = "report"
 
     id_report = db.Column(db.Integer, primary_key=True)
-    id_post = db.Column(db.Integer, db.ForeignKey("post.id_post"), nullable=False)
+    id_post = db.Column(db.Integer, db.ForeignKey(
+        "post.id_post"), nullable=False)
     content = db.Column(db.String(120), nullable=False, default='')
-    reg_student = db.Column(db.Integer, db.ForeignKey("student.reg_student"), nullable=False)
-    report_type = db.Column(db.Enum(ReportTypes), nullable=False)
+    reg_student = db.Column(db.Integer, db.ForeignKey(
+        "student.reg_student"), nullable=False)
+    offensive = db.Column(db.Boolean, nullable=False)
+    prejudice = db.Column(db.Boolean, nullable=False)
+    unrelated = db.Column(db.Boolean, nullable=False)
+    others = db.Column(db.Boolean, nullable=False)
