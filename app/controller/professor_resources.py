b3344964451be5799e58822306a797ba1bcc277c
Victor Hugo <hugo.victorcsi@gmail.com>
2021-04-28 22:23:28 -0300

    (#180) Adicionando a classe ProfessorIdDatail para pegar post por id de professor
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>

--- app/controller/professor_resources.py
+++ app/controller/professor_resources.py
@@ -9,8 +9,12 @@ from marshmallow import ValidationError
 class ProfessorDetail(Resource):
     @jwt_required()
     def get(self, name):
+        validate = professor_services.validate_name(name)
+        if validate:
+            return make_response(jsonify(validate), 400)
         professors = professor_services.get_professor_name_contains(name)
-        ps = ProfessorSchema(many=True, exclude=['email', 'reg_professor'], context={'reg_student': get_jwt_identity()})
+        ps = ProfessorSchema(many=True, exclude=['email', 'reg_professor', 'posts'], context={
+                             'reg_student': get_jwt_identity()})
         return make_response(ps.jsonify(professors), 200)
 
 
@@ -26,6 +30,20 @@ class ProfessorList(Resource):
             return make_response(jsonify(err.messages), 400)
 
 
+class ProfessorIdDetail(Resource):
+    @jwt_required()
+    def get(self, id):
+        try:
+            ProfessorSchema(only=['id_professor']).load({'id_professor': id})
+            professor = professor_services.get_professor_id(id)
+            ps = ProfessorSchema(many=True, exclude=['email', 'reg_professor'], context={
+                                 'reg_student': get_jwt_identity()})
+            return make_response(ps.jsonify(professor), 200)
+        except ValidationError as err:
+            return make_response(jsonify(err.messages), 400)
+
+
 def configure(api):
     api.add_resource(ProfessorList, "/professor")
     api.add_resource(ProfessorDetail, "/professor/<string:name>")
+    api.add_resource(ProfessorIdDetail, "/professor/<int:id>")
