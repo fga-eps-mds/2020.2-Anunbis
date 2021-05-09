8d00e53cb554426ae9897e9558b6d98eaf982635
thiagohdaqw <thiagohdaqw@gmail.com>
2021-05-09 15:16:43 -0300

    (#200) adicionando exclusao da migracoes

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,12 +2,13 @@
 
 black --version
 if [[ "$?" != "0" ]]; then
-    pip install black --quiet
+    pip3 install black --quiet
 fi
 flake8 --version
 if [[ "$?" != "0" ]]; then
-    pip install flake8 --quiet
+    pip3 install flake8 --quiet
 fi
 
-black ./
-flake8 --max-line-length=88
+black ./app ./tests 
+flake8 --max-line-length=88 --exclude migrations
+
