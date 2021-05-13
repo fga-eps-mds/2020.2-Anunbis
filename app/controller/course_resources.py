1f91ef828195673ab2beae729bee173196375c73
Rodrigo Balbino <rodrigo080599@hotmail.com>
2021-05-13 00:32:57 -0300

    (#204)Adicionando o docstring da rota GET do courselist

--- app/controller/course_resources.py
+++ app/controller/course_resources.py
@@ -6,10 +6,20 @@ from ..schemas.course_schema import CourseSchema
 
 class CourseList(Resource):
     def get(self):
+        """
+        This path is responsable for getting the list of courses.
+        ---
+        tags:
+        - Course's paths
+        responses:
+            200:
+                description: It will return all the courses, if there's any
+
+        """
         courses = course_services.get_course()
         cs = CourseSchema(many=True)
         return make_response(cs.jsonify(courses), 200)
 
 
 def configure(api):
-    api.add_resource(CourseList, "/course")
+    api.add_resource(CourseList, "course")
