1509cb9aa0414a4ceff7d6f36a8bea204236ab5a
fellipepcs <fellipy.02@hotmail.com>
2022-08-10 18:44:26 -0300

    Adicionado casos de testes

---
+++ tests/tests_user_service.py
@@ -0,0 +1,52 @@
+from app.schemas.user_schema import UserSchema
+import unittest
+
+
+class TestUser(unittest.TestCase):
+    '''Deve lançar um erro caso o email seja nulo'''
+    def test_validate_email_none(self):
+        try:
+            UserSchema().validate_email(None)
+            self.assertTrue(True)
+        except:
+            self.assertFalse(False)
+
+    '''Deve lançar um erro caso o email seja do tipo inteiro'''
+    def test_validate_email_number(self):
+        try:
+            UserSchema().validate_email(2)
+            self.assertTrue(True)
+        except:
+            self.assertFalse(False)
+
+    '''Deve lançar um erro caso o email seja invalido o formato'''
+    def test_validate_email_invalid(self):
+        try:
+            UserSchema().validate_email('fellipe')
+            self.assertTrue(True)
+        except:
+            self.assertFalse(False)
+
+    '''Deve aceitar o caso se email for valido'''
+    def test_validate_email_correto(self):
+        try:
+            UserSchema().validate_email('fellipe@gmail.com')
+            self.assertFalse(False)
+        except:
+            self.assertTrue(True)
+
+    '''Deve lançar um erro caso o email seja true'''
+    def test_validate_email_True(self):
+        try:
+            UserSchema().validate_email(True)
+            self.assertTrue(True)
+        except:
+            self.assertFalse(False)
+    
+    '''Deve lançar um erro caso o email seja false'''
+    def test_validate_email_False(self):
+        try:
+            UserSchema().validate_email(False)
+            self.assertTrue(True)
+        except:
+            self.assertFalse(False)
